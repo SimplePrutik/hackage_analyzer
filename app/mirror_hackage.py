bbcde66ac19a989eddb1cdb82abe9ffc63fc623c
SimplePrutik <dimodaktil@yandex.ru>
2017-06-13 15:37:35 +0300

    final version

--- app/mirror_hackage.py
+++ app/mirror_hackage.py
@@ -9,27 +9,24 @@ packages = json.loads(r.text)
 
 if not os.path.exists('../hackage/'):
     	os.makedirs('../hackage/')
-count = 0
 
 for pkg in packages:
 	try:
 		cabal = (requests.get("https://hackage.haskell.org/package/" + pkg['packageName'] + "/" + pkg['packageName'] + ".cabal")).text
 	except:
+		print("https://hackage.haskell.org/package/" + pkg['packageName'] + "/" + pkg['packageName'] + ".cabal")
 		print(pkg['packageName'] + ' get_request couldnt be made')
-	
+	cabal = re.sub(re.compile("--.*?\n" ) ,"" ,cabal)
 	try:
-		version = re.search('(v|V)ersion *:? *\r?\n? *((\d.)*\d)', cabal).group(2)
+		version = re.search('(v|V)ersion\s*:?\s*(\d+(\.\d*)*)', cabal).group(2)
 	except:
 		print(pkg['packageName'] + ' smthg wrong with version')
-
 	try:
 		urllib.request.urlretrieve('https://hackage.haskell.org/package/' + pkg['packageName'] + '-' + version + '/' + pkg['packageName'] + '-' + version + '.tar.gz', '../hackage/' + pkg['packageName'] + '-' + version + '.tar.gz')
 	except:
 		print(pkg['packageName'] + ' download was interrupted')
+		print('-----------------------------------------#')
 
-	if count == 10:
-		break
-	count = count + 1
 	
 
 	
